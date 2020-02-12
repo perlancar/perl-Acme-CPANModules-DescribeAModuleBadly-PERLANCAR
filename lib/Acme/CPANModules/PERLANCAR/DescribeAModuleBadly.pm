f722a299a4fe93c59bb49b91f8859b7ca2a29d2d
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2020-02-12 19:39:12 +0700

    Initial commit

---
+++ lib/Acme/CPANModules/PERLANCAR/DescribeAModuleBadly.pm
@@ -0,0 +1,20 @@
+package Acme::CPANModules::PERLANCAR::DescribeAModuleBadly;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+our $LIST = {
+    summary => 'PERLANCAR describing modules badly',
+    description => <<'_',
+
+Inspired by Jimmy Fallon's Twitter hashtag #DescribeAMovieBadly (Feb 4, 2020).
+
+_
+    entries => [
+    ],
+};
+
+1;
+# ABSTRACT:
