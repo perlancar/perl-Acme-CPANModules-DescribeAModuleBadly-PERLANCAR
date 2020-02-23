a8ce250b81f8ee413d962b974f85b742fd769f72
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2020-02-23 12:49:31 +0700

    Rename module/dist, add some entries

---
+++ lib/Acme/CPANModules/DescribeAModuleBadly/PERLANCAR.pm
@@ -0,0 +1,57 @@
+package Acme::CPANModules::DescribeAModuleBadly::PERLANCAR;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+our $LIST = {
+    summary => 'PERLANCAR describing modules badly',
+    description => <<'_',
+
+_
+    entries => [
+
+        {
+            module => 'Path::Tiny',
+            description => <<'_',
+
+When you're bored and want to relearn crawling, standing up, walking and speech
+all over again when dealing with files.
+
+_
+        },
+
+        {
+            module => 'M',
+            description => <<'_',
+
+Remember when you want to have more than four ways to do OO, but less than six?
+Yeah, me neither.
+
+ (To be honest, I've used all <pm:Moose>, <pm:Mouse>, <pm:Moo>,
+and <pm:Mo> for "real-world" purposes, and Mo is so lightweight the only way to
+be more lightweight is to do bare OO by yourself.)
+
+_
+        },
+
+        {
+            module => 'Dist::Zilla',
+            description => <<'_',
+
+An ideal distribution builder tool, particularly if you need to develop dzil
+plugin distributions.
+
+_
+        },
+
+    ],
+};
+
+1;
+# ABSTRACT:
+
+=head1 append:SEE ALSO
+
+The L<Acme::CPANModules::DescribeAModuleBadly> namespace.
